1d1ab9b1e2ca05185ac9ee3c4e458494b135828f
Adrian <adrian.jiwantoro@gmail.com>
2020-05-12 11:19:32 +0700

    [feat] Update router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ import colors from 'vuetify/es5/util/colors'
 // only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
 const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
   router: {
-    base: '/ryuzaki201.github.io/'
+    base: '/nuxt-github-pages/'
   }
 } : {}
 
